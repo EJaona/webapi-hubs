35b2ca8076eab3cd58aaf580a9e7e516ce401cd1
Enoka Jaona <Ejaona@gmail.com>
2019-02-11 17:08:17 -0600

    post endpoint working

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(express.json());
 server.post("/hubs", (req, res) => {
   const hub = req.body;
 
-  db.hub
+  db.hubs
     .add(hub)
     .then(hub => {
       res.status(201).json({ success: true, hub });
@@ -22,7 +22,7 @@ server.post("/hubs", (req, res) => {
 });
 
 // The R in CRUD
-server.get("/", (req, res) => {
+server.get("/hubs", (req, res) => {
   db.hubs
     .find()
     .then(hubs => {
@@ -34,6 +34,26 @@ server.get("/", (req, res) => {
 });
 
 // The U in CRUD
+server.put("/hubs/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db.hubs
+    .update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.status(200).json({ success: true, updated });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "I cannot find the hub you are looking for"
+        });
+      }
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ success: false, message });
+    });
+});
 
 // The D in CRUD
 server.delete("/hubs/:id", (req, res) => {
